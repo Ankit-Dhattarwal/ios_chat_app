89a9c71703d768ae8c1dcc34dfac8fa4fb29a084
AnkitDhattarwal <ankitdhatterwal09@gmail.com>
2026-01-22 01:00:41 +0530

    add tableView

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -8,10 +8,18 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    
+    var messages: [Message] = [
+        Message(sender: "ankit@1", body: "Hi"),
+        Message(sender: "ankit@1", body: "How are you"),
+        Message(sender: "ankit@1", body: "What'sApp!")
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "⚡️FlashChat"
+//        tableView.delegate = self
+        tableView.dataSource = self
+        title = Constants.appName
         navigationItem.hidesBackButton = true
     }
     
@@ -28,3 +36,24 @@ class ChatViewController: UIViewController {
         }
     }
 }
+
+
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell;
+    }
+    
+    
+}
+
+//extension ChatViewController: UITableViewDelegate{
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//}
